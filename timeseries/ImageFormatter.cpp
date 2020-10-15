4874cd521781678c1db301c2a77f709d3725b83e
Mika Heiskanen <mika.heiskanen@fmi.fi>
2020-10-15 09:10:48 +0300

    Use new TableFormatter API

--- timeseries/ImageFormatter.cpp
+++ timeseries/ImageFormatter.cpp
@@ -6,6 +6,7 @@
 
 #include "ImageFormatter.h"
 #include <spine/Convenience.h>
+#include <spine/HTTP.h>
 #include <macgyver/Exception.h>
 #include <spine/Table.h>
 #include <boost/algorithm/string/classification.hpp>
@@ -44,11 +45,10 @@ std::set<std::string> parse_debug_attributes(const std::string& theStr)
  */
 // ----------------------------------------------------------------------
 
-void ImageFormatter::format(std::ostream& theOutput,
-                            const Table& theTable,
-                            const TableFormatter::Names& theNames,
-                            const HTTP::Request& theReq,
-                            const TableFormatterOptions& /* theConfig */) const
+std::string ImageFormatter::format(const Table& theTable,
+                                   const TableFormatter::Names& theNames,
+                                   const HTTP::Request& theReq,
+                                   const TableFormatterOptions& /* theConfig */) const
 {
   try
   {
@@ -63,38 +63,43 @@ void ImageFormatter::format(std::ostream& theOutput,
     const Table::Indexes cols = theTable.columns();
     const Table::Indexes rows = theTable.rows();
 
-    theOutput << "<!DOCTYPE html><html><head><title>Debug mode output</title>"
-              << "<style>"
-              << "table {border-collapse: collapse;}"
-              << "th, td {border-bottom: 1px solid black; padding: 3px 0.5em 3px 0.5em; "
-              << "text-align: center;}"
-              << "tr:nth-child(even) {background-color: #f2f2f2;}"
-              << "tr:hover {background-color: #e2e2e2;}"
-              << "</style>" << std::endl
-              << "</head><body>" << std::endl;
+    std::string out= "<!DOCTYPE html><html><head><title>Debug mode output</title>"
+                     "<style>"
+                     "table {border-collapse: collapse;}"
+                     "th, td {border-bottom: 1px solid black; padding: 3px 0.5em 3px 0.5em; "
+                     "text-align: center;}"
+                     "tr:nth-child(even) {background-color: #f2f2f2;}"
+                     "tr:hover {background-color: #e2e2e2;}"
+                     "</style>\n"
+                     "</head><body>\n";
 
     // Output headers
 
-    theOutput << "<table><tr>";
+    out += "<table><tr>";
     for (const auto& nam : cols)
     {
       const std::string& name = theNames[nam];
-      theOutput << "<th>" << htmlescape(name) << "</th>";
+      out += "<th>";
+      out += htmlescape(name);
+      out += "</th>";
     }
-    theOutput << "</tr>";
+    out += "</tr>";
 
     for (const auto j : rows)
     {
-      theOutput << "<tr>" << std::endl;
+      out += "<tr>\n";
       for (const auto i : cols)
       {
         //std::string value = htmlescape(theTable.get(i, j));
         std::string value = theTable.get(i, j);
-        theOutput << "<td>" << (value.empty() ? miss : value) << "</td>";
+        out += "<td>";
+        out += (value.empty() ? miss : value);
+        out += "</td>";
       }
-      theOutput << "</tr>";
+      out += "</tr>";
     }
-    theOutput << "</table></body></html>";
+    out += "</table></body></html>";
+    return out;
   }
   catch (...)
   {
